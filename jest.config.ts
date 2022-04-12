219266b7afda2def1297b64ab5c4b8deff215358
neilcochran <neil.cochran@pm.me>
2022-04-11 21:22:04 -0400

    add jest support - just plumbing and a single dummy test

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+        '**/test/**/*.test.ts'
+    ],
+
+    // Indicates whether each individual test should be reported during the run
+    verbose: true
+};
